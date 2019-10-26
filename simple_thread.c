fc6f83b348c0f7955f4da45765fa11e3d5dcb24c
John Chen <johnchen77w@gmail.com>
2019-10-26 00:19:32 -0400

    Update simple_thread.c

--- simple_thread.c
+++ simple_thread.c
@@ -4,228 +4,83 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <errno.h>
-#include <string.h>
 #include <pthread.h>
 
-#define BUFFER_MAX_LENGTH 1024
-#define USER_SIZE 10
+// initializing variables
+int x = 10;
+int y = 20;
+int z = 0;
 
 /*
-    Main fcfs function
-*/
-int fcfs(char **arr_fcfs, int pCounter, FILE *new_file)
+ * Function:  thread_calculate
+ * --------------------
+ * This is a thread function, which computes and prints the value of z using:
+ *    z = x + y
+ *
+ *  z: integer pointer
+ *
+ *  returns: 0
+ */
+void *thread_calculate(void *z)
 {
-    int finalTime = 0;
-    int totalTime = 0;
-    fprintf(new_file, "        Order of selection by CPU:  \n\n");
-    fprintf(new_file, "            ");
-    for (int i = 0; i < pCounter; i++) {
-        fprintf(new_file, "P%d ", i+1);
-    }
-    fprintf(new_file, "\n\n        Individual waiting times for each process: \n\n");
-
-    for (int j = 0; j < pCounter; j++) {
-        fprintf(new_file, "            P%d = %d\n", j+1, finalTime);
-        finalTime = finalTime + atoi(arr_fcfs[j]);
-        totalTime = totalTime + finalTime;
-    }
-    totalTime = totalTime - finalTime;
-    fprintf(new_file, "\n        Average waiting time = %0.1f \n\n", (double)totalTime/pCounter);
+    *(int *)z = x+y;
+    printf("From thread_calculate: x + y = z = %d\n\n", *(int *)z);
     return 0;
 }
 
-
-
 /*
-    A struct that defines two variables for the original value and their index
-*/
-struct sjf_struct
-{
-    int value;
-    int index;
-};
+ * Function:  main
+ * --------------------
+ * This the main function, which does the following:
+ *      fork()
+ *      calculating z value
+ *      making new thread
+ *
+ *  returns: 0
+ */
+int main() {
+    pid_t pid1;
+    printf("\n- - - - - - - - - - - - S - T - A - R - T - - - - - - - - - - - - \n\n");
+    pid1 = fork();
 
-/*
-    A helper function for sjf, compare two pointers and set return accordingly
-*/
-int sjf_cmp(const void *a, const void *b)
-{
-    struct sjf_struct *a1 = (struct sjf_struct *)a;
-    struct sjf_struct *a2 = (struct sjf_struct *)b;
-    if ((*a1).value < (*a2).value)
-        return -1;
-    else if ((*a1).value > (*a2).value)
-        return 1;
-    else
-        return 0;
-}
-
-/*
-    Main sjf function, using qsort() to sort the array elements in ascending order
-*/
-int sjf(char **arr_sjf, int pCounter, FILE *new_file)
-{
-    char *temp;
-    int finalTime = 0;
-    int totalTime = 0;
-    struct sjf_struct objects[pCounter];
-    for (int i = 0; i < pCounter; i++)
+    // fork unsuccessful :(
+    if (pid1 < 0)
     {
-        objects[i].value = atoi(arr_sjf[i]);
-        objects[i].index = i;
-    }
-    qsort(objects, pCounter, sizeof(objects[0]), sjf_cmp);
-    fprintf(new_file, "        Order of selection by CPU:  \n\n");
-    fprintf(new_file, "            ");
-    for (int i = 0; i < pCounter; i++)
-    {
-        fprintf(new_file,      "P%d ", objects[i].index + 1); //will give 1 2 0
-    }
-    fprintf(new_file, "\n\n        Individual waiting times for each process:   \n\n");
-    for (int i = 0; i < pCounter; i++)
-    {
-        for (int j = i + 1; j < pCounter; j++)
-        {
-            if (atoi(arr_sjf[i]) > atoi(arr_sjf[j]))
-            {
-                temp = arr_sjf[i];
-                arr_sjf[i] = arr_sjf[j];
-                arr_sjf[j] = temp;
-            }
-        }
-        fprintf(new_file, "            P%d = %d\n", objects[i].index + 1, finalTime);
-        finalTime = finalTime + atoi(arr_sjf[i]);
-        totalTime = totalTime + finalTime;
-    }
-    totalTime = totalTime - finalTime;
-    fprintf(new_file, "\n        Average waiting time = %0.1f \n\n", (double)totalTime/pCounter);
-    return 0;
-}
-
-
-/*
-    Main rr function
-*/
-int rr(char **arr_rr, int pCounter, int tq, FILE *new_file)
-{
-    int finished = 0; // 0 = finished, 1 = not finished yet
-    int process_remains;
-    int pQueue [pCounter]; 
-    fprintf(new_file, "            Order of selection by CPU:  \n\n");
-    fprintf(new_file, "                ");
-    for (int i = 0; i < pCounter; i++)
-    {
-        pQueue[i] = atoi(arr_rr[i]);
-        fprintf(new_file, "P%d ", i + 1);
-    }
-    while (finished == 0)
-    {
-        process_remains = pCounter;
-        for (int i = 0; i < pCounter; i++)
-        {
-            if (pQueue[i] > tq)
-            {
-                pQueue[i] = pQueue[i] - tq;
-                fprintf(new_file, "P%d ", i + 1);
-            }
-            else if (pQueue[i] < tq)
-            {
-                process_remains --;
-            }
-            else if (pQueue[i] == tq)
-            {
-                process_remains --;
-            }
-            if (process_remains == 0)
-            {
-                finished = 1;
-            }
-        }
-    }  
-    fprintf(new_file, "\n\n");
-    return 0;
-}
-
-
-
-
-int main(int argc, char const *argv[])
-{
-    int processTime, processNum, timeQuantum;
-    int row = 2, col;
-    int lineCounter = 0;
-    char const* const fileName = argv[1];
-    char line [BUFFER_MAX_LENGTH];
-    FILE *file;
-    FILE *new_file;
-    char *split;
-    char delim[] = " ";
-    char *lineArr[BUFFER_MAX_LENGTH];
-    char *splitArr[BUFFER_MAX_LENGTH];
-    char *processArr[BUFFER_MAX_LENGTH];
-    file = fopen("cpu_scheduling_input_file.txt", "r");
-    if (!file)
-    {
-        fprintf(stderr, "error: could not open textfile: cpu_scheduling_input_file.txt\n");
-        return EXIT_FAILURE;
-    }
-    new_file = fopen("cpu_scheduling_output_file.txt", "a");
-    if (!new_file)
-    {
-        fprintf(stderr, "error: could not open textfile: cpu_scheduling_output_file.txt\n");
-        return EXIT_FAILURE;
-    }
-    while (fgets(line, sizeof(line), file) != NULL) 
-    {
-        lineArr[lineCounter] = malloc(sizeof(line));
-        strcpy(lineArr[lineCounter], line);
-        lineCounter++;
-    }
-    fprintf(new_file, "- - - - - - - - - - - - - - - - - - - - -  S - T - A - R - T - - - - - - - - - - - - - - - - - - - - - \n\n");
-    fprintf(new_file, "%d ready queues have been read and stored\n", lineCounter);
-    for (size_t j = 0; j < lineCounter; j++) 
-    {
-        fprintf(new_file, "\n- - - - - - - - - - - - Q - U - E - U - E - - %d - - - - - - - - - - - - \n\n", j + 1);
-        // first, count number of P's in one line to determine its number of processes
-        int pCounter = 0;
-        for (size_t k = 0; k < strlen(lineArr[j]); k++) 
-        {
-            if(lineArr[j][k] == 'p')
-            pCounter++;
-        }
-        fprintf(new_file, "Number of processes: %d\n\n", pCounter);
-        int splitCounter = 0;
-        split = strtok (lineArr[j], delim);
-        while (split != NULL) 
-        {
-            splitArr[splitCounter] = malloc(strlen(lineArr[j]));
-            strcpy(splitArr[splitCounter], split);
-            splitCounter++;
-            split = strtok(NULL, delim);
-        }
-        timeQuantum = atoi(splitArr[3]);
-        fprintf(new_file, "Time quantum: %d\n\n", timeQuantum);
-        // another array corresponding to its process times
-        int proCounter = 0;
-        for (int p = 5; p < splitCounter; p = p + 2) 
-        {
-        processArr[proCounter] = (char*)malloc(strlen(splitArr[p]) + 1);
-        strcpy(processArr[proCounter], splitArr[p]);
-        proCounter++;
-        }
-        fprintf(new_file, "    Ready Queue %lu Applying FCFS Scheduling:\n\n", j+1);
-        fcfs(processArr, proCounter, new_file);
-        fprintf(new_file, "    Ready Queue %zu Applying RR Scheduling...\n\n", j+1);
-        rr(processArr, proCounter, timeQuantum, new_file);
-        fprintf(new_file, "    Ready Queue %zu Applying SJF Scheduling...\n\n", j+1);
-        sjf(processArr, proCounter, new_file);
-
+        // prints out error message
+        perror("fork unsuccessful");
+    	exit(1);
     }
 
+    // child process :)
+    if (pid1 == 0)
+    {
+        printf("    Child process (PID %d):\n\n", getpid());
+        // calculating z =  x + y
+        z = x + y;
+        printf("    Performing x + y ...\n\n");
+    }
 
-    fclose(new_file);
-    fclose(file);
+    // parent process :)
+    if (pid1 > 0)
+    {
+        printf("Parent process (PID %d): \n\n", getpid());
+        // wait for child to complete
+        wait(NULL);
+        // prints out the result
+        // the value of z will remain unchanged (0) since child only modified
+        //  its own copy of z value
+        printf("x + y = z = %d\n\n", z);
+        // make new thread from parent
+        pthread_t thread_1;
+        printf("From parent: creating thread...\n\n");
+        // calling thread function
+        pthread_create(&thread_1, NULL, thread_calculate, (void *)&z);
+        // suspend execution of the calling thread until the target thread terminates
+        pthread_join(thread_1, NULL);
+        // print after thread terminates
+        printf("Thread terminated\n\n");
+        printf("- - - - - - - - - - - - - - E - N - D - - - - - - - - - - - - - - \n\n");
+    }
 
     return 0;
 }
-
